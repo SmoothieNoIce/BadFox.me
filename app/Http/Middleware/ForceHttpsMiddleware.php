f888e5b8411dbebfbf91b5d9ffae65b32f4b711a
SmoothieNoIce <aci5722@gmail.com>
2019-11-13 12:22:09 +0800

    w\

--- app/Http/Middleware/ForceHttpsMiddleware.php
+++ app/Http/Middleware/ForceHttpsMiddleware.php
@@ -7,10 +7,7 @@ use Closure;
 class ForceHttpsMiddleware{
     public function handle($request, Closure $next){
 
-        if (!$request->secure()) {//optionally disable for localhost development
-            $request->setTrustedProxies( [ $request->getClientIp() ] ); 
-            return redirect()->secure($request->getRequestUri());
-        }
+    
 
         return $next($request);
     }
