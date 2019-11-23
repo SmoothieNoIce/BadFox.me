3d66dea976d1f3cfc8d60021c7877137a905ca4b
SmoothieNoIce <aci5722@gmail.com>
2019-11-23 15:34:46 +0800

    w

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -6,11 +6,11 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Article;
 use Illuminate\Support\Facades\DB;
+use Response;
 
 class ArticleController extends Controller
 {
     public function getFirst4Article(){
-        $articles = Article::all()->take(4);
-        return dd($articles);
+        return "H";
     }
 }
