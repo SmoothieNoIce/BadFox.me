050af85902e117815f182b26fd503a44b415e320
SmoothieNoIce <aci5722@gmail.com>
2019-11-23 15:18:41 +0800

    apiV1

---
+++ app/Http/Controllers/Api/ArticleController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Article;
+use Illuminate\Support\Facades\DB;
+
+class ArticleController extends Controller
+{
+    public function getFirst4Article(){
+        $articles = Article::all()->take(4);
+        return $articles->toJson();
+    }
+}
